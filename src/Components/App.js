676d614a00ac591f13450be112348e16e93ec014
Cody <codybarker.or@gmail.com>
2024-09-05 15:15:10 -0700

    add initial navbar and styling

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,11 @@
 import "../Styles/App.css";
+import NavBar from "./NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <NavBar />
+      <header className="header"></header>
     </div>
   );
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__item">
+          <NavLink to="/" className="nav__link">
+            Home
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink to="#about" className="nav__link">
+            About
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink to="#projects" className="nav__link">
+            Projects
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink to="#socials" className="nav__link">
+            Socials
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
