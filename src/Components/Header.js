cdad8421cdd727f4deec2a735d350c758ef5fb1b
Cody <codybarker.or@gmail.com>
2024-09-05 16:47:48 -0700

    style header and add media queries

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,23 @@
 function Header() {
-  return;
-  <header className="header"></header>;
+  return (
+    <div className="header__container">
+      <header className="header">
+        <img
+          className="header__image"
+          alt="developer headshot"
+          src="/assets/headshot-full-res.png"
+        />
+        <div className="header__content">
+          <h1 className="header__title">Cody Barker</h1>
+          <p className="header__text">
+            Creative software solutions to optimize your business and enhance
+            the user experience.
+          </p>
+          <button className="header__button">Resume</button>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default Header;
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,11 +5,6 @@ function NavBar() {
     <nav className="nav">
       <ul className="nav__list">
         <li className="nav__item">
-          <NavLink to="/" className="nav__link">
-            Home
-          </NavLink>
-        </li>
-        <li className="nav__item">
           <NavLink to="#about" className="nav__link">
             About
           </NavLink>
