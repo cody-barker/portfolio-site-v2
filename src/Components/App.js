85b8da277cbcebcb2f38cd207b6af57e58d75b4e
Cody <codybarker.or@gmail.com>
2024-09-05 15:53:56 -0700

    center nav

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,12 @@
 import "../Styles/App.css";
 import NavBar from "./NavBar";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
       <NavBar />
-      <header className="header"></header>
+      <Header />
     </div>
   );
 }
