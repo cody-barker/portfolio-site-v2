6d9faa01278804096883cb8217e2ae2455d88a33
Cody <codybarker.or@gmail.com>
2024-09-06 10:21:46 -0700

    complete Project and ProjectGroup

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,5 +1,13 @@
-function Project() {
-  return <div>Project</div>;
+function Project({ title, subtitle, image, alt }) {
+  return (
+    <div className="project">
+      <img className="project__image" src={image} alt={alt} />
+      <div className="project__content">
+        <h3 className="project__title">{title}</h3>
+        <p className="project__subtitle">{subtitle}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Project;
--- src/Components/ProjectGroup.js
+++ src/Components/ProjectGroup.js
@@ -7,38 +7,43 @@ function ProjectGroup() {
       subtitle:
         "Custom inventory management software designed to streamline record keeping and ensure secure handling of hazardous chemicals.",
       image: "/assets/acfm-pesticide-inventory-app-demo-preview.png",
+      alt: "Preview of the Ash Creek Forest Management chemical inventory application.",
     },
     {
       title: "Soil Moisture Sensors for Ash Creek Nursery",
       subtitle:
         "Specialized soil moisture sensors decrease water consumption and improve plant health at a local native plant nursery.",
       image: "/assets/soil-moisture-sensors-preview.png",
+      alt: "Preview of the soil moisture sensor dashboard in Grafana.",
     },
     {
       title: "Portland Native Plants",
       subtitle:
         "An extensive native plant resource that guides project managers and home gardeners in making informed plant selections.",
       image: "/assets/portland-native-plants-preview.png",
+      alt: "Preview of the native plants table in this web app.",
     },
     {
       title: "Addressable LED Rock Climbing Wall",
       subtitle:
         "This WebSocket application and lighting controller empowers users to create, save, and share personalized climbing routes at a fraction of the cost of commercial alternatives.",
       image: "/assets/led-rock-wall-app-preview.png",
+      alt: "Preview of the websocket UI for controlling addressable LEDs.",
     },
     {
       title: "Trailscan",
       subtitle:
         "A web app designed for hikers to add and share their favorite trails with ease.",
       image: "/assets/trailscan-preview-image.png",
+      alt: "Preview of the Trailscan homepage.",
     },
   ];
 
-  const projectCards = projects.map((project) => {
-    <Project />;
+  const projectCards = projects.map((project, index) => {
+    return <Project key={index} {...project} />;
   });
 
-  return <div>ProjectGroup</div>;
+  return { projectCards };
 }
 
 export default ProjectGroup;
