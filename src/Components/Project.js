6fd1ca562121e16e014fa652446dba471af7107d
Cody <codybarker.or@gmail.com>
2024-10-08 10:57:17 -0700

    lazy load project images

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,7 +1,7 @@
 function Project({ title, subtitle, image, alt }) {
   return (
     <div className="project flex__column--center">
-      <img className="project__image" src={image} alt={alt} />
+      <img className="project__image" src={image} alt={alt} loading="lazy" />
       <div className="project__content flex__column--center">
         <h3 className="project__title">{title}</h3>
         <p className="project__subtitle">{subtitle}</p>
