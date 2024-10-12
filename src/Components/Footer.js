9f017111731e428412d0e8a70e01e28378f1f91c
Cody <codybarker.or@gmail.com>
2024-10-12 11:19:48 -0700

    update footer structure and center it

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,15 @@
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer__content">
-        <h4 className="footer__heading">Contact Me</h4>
-      </div>
-      <div className="footer__content">
-        <h4 className="footer__heading">Socials</h4>
-      </div>
-    </footer>
+    <div className="footer-wrapper">
+      <footer className="footer">
+        <div className="footer__content">
+          <h4 className="footer__heading">Contact Me</h4>
+        </div>
+        <div className="footer__content">
+          <h4 className="footer__heading">Socials</h4>
+        </div>
+      </footer>
+    </div>
   );
 }
 
