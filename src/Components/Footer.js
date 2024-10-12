1a14d3c5ff1f2ac23fee9a0048275d86c0c01369
Cody <codybarker.or@gmail.com>
2024-10-12 11:08:32 -0700

    update footer with content blocks

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,14 @@
 function Footer() {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <h4 className="footer__heading">Contact Me</h4>
+      </div>
+      <div className="footer__content">
+        <h4 className="footer__heading">Socials</h4>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
