0a218a8d22f098c070cf00d615a49ff0aad0a24d
Cody <codybarker.or@gmail.com>
2024-10-12 15:46:12 -0700

    update about with photo and styling

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,6 +3,19 @@ function About() {
     <div className="about-wrapper">
       <div className="about">
         <h1 className="about__title">About Me</h1>
+        <div className="about__content">
+          <img
+            className="about__image"
+            src="/assets/cody_greenhouse.jpg"
+            alt="cody in the greenhouse"
+          />
+          <p className="about__text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui earum
+            nobis atque corporis voluptatibus tempore fugit voluptas vero velit
+            explicabo asperiores molestiae modi, distinctio consectetur dicta
+            beatae. Aut, est enim.
+          </p>
+        </div>
       </div>
     </div>
   );
